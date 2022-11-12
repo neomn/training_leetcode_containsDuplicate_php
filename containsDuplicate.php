9442c0faf0db25555ffe342eefa5bf8f6ceaa967
neomn110 <neomn110@gmail.com>
2022-11-12 22:20:24 +0330

    feat: add leetcode template

--- containsDuplicate.php
+++ containsDuplicate.php
@@ -1,1 +1,15 @@
 <?php
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @return Boolean
+     */
+    public function containsDuplicate($nums)
+    {
+
+    }
+}
+
+$solution = new Solution();
+$solution->containsDuplicate(5);
